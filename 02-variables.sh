9d518ad07d9baf12b71fd65f80601502e1d4741d
CathGilb <catou_gilbert@hotmail.com>
2023-05-01 09:56:45 -0400

    Variables terminées

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+prenom=Neo #Pas d'espaces avant et après le =, guillemets nécessaire juste si des espaces dans la String
+echo $prenom
+
+declare -i age=33 #Pour forcer un type de variable, très peu utilisé.
+declare -r Chemin=/bin #-r pour une constante
+
+echo "Nom script" $0
+echo "Nombre param" $#
+echo "Perso : " $1 #Pour appeler le premier paramètre passé dans le script
