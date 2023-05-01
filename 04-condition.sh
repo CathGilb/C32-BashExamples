5c50b57ed40a766fb8f8634076f414c74ec185ef
CathGilb <catou_gilbert@hotmail.com>
2023-05-01 10:25:41 -0400

    Conditions terminées

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
+note=70
 
+#if test ...
+#if [...]
 
+if [[ $note -lt 60 ]] #ou ;then... pour être sur la même ligne
+then
+    echo "Désolé... Vous échouez."
+elif test $note -eq 60
+then
+    echo "Ouf!"
+else
+    echo "Beau travail!"
+fi
 
+read -p "Entrez une lettre" lettre
 
+case $lettre in 
+    [[:lower:]])
+        echo "la lettre est en minuscule"
+        ;;
+    *)
+        echo "Autre"
+    ;;
+esac
 
-
-
+if [[ $lettre != "a" ]] #On utilise les caractères pour les chaines de caractères
+then    
+    echo "diff de a"
+fi
