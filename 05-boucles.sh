826ccd9bc90d2fedd289533f8bb95fcb1198e705
CathGilb <catou_gilbert@hotmail.com>
2023-05-01 10:43:45 -0400

    Boucles terminées

--- 05-boucles.sh
+++ 05-boucles.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 
+for ((i = 0; i < 5 ; i++))
+do
+    echo $i
+done
+
+#`` pour exécuter une commande et mettre le résultat dans une variable
+liste=`ls` #MAJ+touche è
+
+for fichier in $liste
+do
+    echo $fichier
+done
+
+resultat=o
+
+while [[ $resultat = o ]]
+do
+    read -p "Choix: " resultat
+done
